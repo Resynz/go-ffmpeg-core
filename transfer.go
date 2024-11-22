3f55f97cb99fe5cb324e465a091eaad70fc7d723
resynz <yuanjianxin1993@gmail.com>
2024-11-22 16:02:23 +0800

    fix transfer

--- transfer.go
+++ transfer.go
@@ -15,12 +15,25 @@ type Transfer struct {
 }
 
 func (s *Transfer) Execute() error {
-	args := make([]string, 0)
-	args = append(args, "-i", s.ResourcePath, "-s", s.Quantity, "-c:v", "h264", "-b:v", s.Bitrate, s.OutputPath)
+	// 确保输出路径以 .mp4 结尾
+	if s.OutputPath[len(s.OutputPath)-4:] != ".mp4" {
+		s.OutputPath += ".mp4"
+	}
+
+	args := []string{
+		"-i", s.ResourcePath,
+		"-s", s.Quantity,
+		"-c:v", "libx264", // H.264 编解码器
+		"-b:v", s.Bitrate, // 视频比特率
+		"-c:a", "aac", // 音频编解码器 (AAC)
+		"-strict", "experimental", // 对于某些 FFmpeg 版本需要添加该参数以启用 AAC
+		s.OutputPath,
+	}
+
 	cmd := exec.Command(s.Command, args...)
 	ot, err := cmd.CombinedOutput()
 	if err != nil {
 		return HandleError(ot)
 	}
-	return err
+	return nil
 }
